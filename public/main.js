925c15b2e29e0dd7c5a75e2d1acaf661bbea1ae7
Tiller <ctiller@proforma.com>
2018-03-15 13:16:04 -0400

    create Deck and Card classes

--- public/main.js
+++ public/main.js
@@ -1,5 +1,82 @@
-const main = () => {
-  document.querySelector('h1').textContent += '?'
+const suiteCards = [2,3,4,5,6,7,8,9,10,'J','Q','K','A'];
+
+class Deck {
+  constructor() {
+
+    // Creates each individual card object.
+    this.generateCards = (suite) => {
+      let cardArr = [];
+      for(let i = 0; i < suiteCards.length; i++) {
+        cardArr.push(new Card(suite, suiteCards[i]));
+      }
+      return cardArr;
+    }
+
+    // Takes all cards and creates a deck out of them.
+    this.createDeck = () => {
+      let tempArr = [];
+      for(let suite in this.suites) {
+        tempArr = tempArr.concat(this.suites[suite]);
+      }
+      return tempArr;
+    }
+
+    // Shuffles the current deck.
+    // Uses the "Durstenfield shuffle".
+    // Picks one random element, and swaps with the current element.
+    // Then picks the next random element from the remainder.
+    this.shuffleDeck = () => {
+      for (let i = this.deck.length - 1; i > 0; i--) {
+        // picking a random element.
+        let j = Math.floor(Math.random() * (i + 1));
+        // Swapping it with the current element.
+        let temp = this.deck[i];
+        this.deck[i] = this.deck[j];
+        this.deck[j] = temp;
+      }
+    }
+
+    // A function that deals cards.
+    // 'num' is the amount of cards to be dealt.
+    // Serves a dual purpose to both 'deal' cards and for when
+    // A user wants to get 'hit' with new cards.
+    this.dealCards = (num) => {
+      let dealtCards = [];
+      for(let i = 0; i < num; i++) {
+        dealtCards.push(this.deck.pop());
+      }
+      console.log(dealtCards);
+    }
+
+    // All of the suites with their respective cards.
+    this.suites = {
+      hearts: this.generateCards('H'),
+      clubs: this.generateCards('C'),
+      spaces: this.generateCards('S'),
+      diamonds: this.generateCards('D'),
+    };
+
+    // the current deck.
+    this.deck = this.createDeck();
+
+  }
 }
 
-document.addEventListener('DOMContentLoaded', main)
+class Card {
+  constructor(suite, value) {
+    this.suite = suite;
+    this.value = value;
+  }
+}
+
+let testDeck = new Deck();
+
+console.log(testDeck.deck);
+
+
+
+const main = () => {
+  document.querySelector('h1').textContent += '?';
+}
+
+document.addEventListener('DOMContentLoaded', main);
